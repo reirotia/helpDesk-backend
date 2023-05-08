185b92e6318517b76953f3941a317a66677c7b20
reirotia <reirotia@gmail.com>
2023-05-08 16:41:58 -0300

    Perfil de Desenvolvimento

--- src/main/java/com/reinaldo/helpDesk/config/DevConfig.java
+++ src/main/java/com/reinaldo/helpDesk/config/DevConfig.java
@@ -8,14 +8,22 @@ import org.springframework.context.annotation.Profile;
 import com.reinaldo.helpDesk.services.DBServices;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBServices dbServices;
 
+	// para pegar o valor da propriedade do application properties
+	//@Value("${}spring.profiles.active")
+	//private String value;
+
 	@Bean
 	public void instaciaDB() {
+
+		// pegando o valor da propriedade do aplicattion properties
+		// if (value.equals("create")) {
+
 		this.dbServices.instanciaDB();
 	}
 
