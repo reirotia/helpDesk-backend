1e2e7c1bbc45a354e8c4e972bd9a8e4cedbd56d6
reirotia <reirotia@gmail.com>
2023-05-07 21:54:48 -0300

    CRIACAO DO ENUM PERFIL

---
+++ src/main/java/com/reinaldo/helpDesk/domain/enums/Perfil.java
@@ -0,0 +1,38 @@
+package com.reinaldo.helpDesk.domain.enums;
+
+public enum Perfil {
+
+	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
+
+	private Integer codigo;
+	private String descricao;
+	
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@SuppressWarnings("unused")
+	private static Perfil toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for (Perfil x : Perfil.values()) {
+			if (cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Perfil invalido");
+	}
+	
+
+}
